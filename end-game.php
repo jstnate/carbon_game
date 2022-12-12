70e84f094a4407d0bf618dfeb553e1b1282fbc87
Ambre Kouitini <ambrektn@outlook.fr>
2022-12-12 10:36:42 +0100

    ADD contact scss file

--- end-game.php
+++ end-game.php
@@ -7,8 +7,7 @@ require_once 'object/connection.php';
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <script src="public/js/endgame.js" defer></script>
         <title>Document</title>
@@ -26,8 +25,8 @@ require_once 'object/connection.php';
                     <input type="checkbox" id="<?= $card['id'] ?>" value="<?= $card['carbon'] ?>" >
                     <label for="<?= $card['id'] ?>">
                         <img
-                                src="https://upload.wikimedia.org/wikipedia/commons/f/f0/Graphite-and-diamond-with-scale.jpg"
-                                alt="" style="width: 100px; aspect-ratio: 4/3"
+                            src="https://upload.wikimedia.org/wikipedia/commons/f/f0/Graphite-and-diamond-with-scale.jpg"
+                            alt="" style="width: 100px; aspect-ratio: 4/3"
                         >
                         <h3><?= $card['card_name']?></h3>
                     <label>
