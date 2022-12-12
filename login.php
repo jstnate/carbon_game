e2a573bd90046620f861de176aa99a422a47e361
Nathan Lefetey <lefetey.nathan@gmail.com>
2022-12-12 14:06:17 +0100

    :lipstick: ADD login page css
    Add login page style

--- login.php
+++ login.php
@@ -11,13 +11,19 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="public/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=KoHo:wght@400;500;700&display=swap" rel="stylesheet">
     <title>Connectez-vous !</title>
 </head>
-<body>
+<body class="login-bg">
     <div class="login">
-        <h1>Connectez-vous !</h1>
+        <div class="login__title">
+            <h1>Bienvenue !</h1>
+            <p>Veuillez vous connecter à votre compte</p>
+        </div>
 
-        <form method="POST">
+        <form class="login__form" method="POST">
             <label for="email">Email</label>
             <input type="email" name="email" id="email" placeholder="mail@gmail.com">
             <label for="password">Mot de passe</label>
