0a55bf67327f3a5e7b85fb4a29a681b258a17e2e
Ambre Kouitini <ambrektn@outlook.fr>
2022-12-12 15:55:36 +0100

    :lipstick: Update contact page style

---
+++ public/component/navbar_contact.php
@@ -0,0 +1,19 @@
+<nav class="navbar">
+    <div class="container-logo">
+        <img class="logo" src="../images/logo.png" alt="">
+    </div>
+    <ul class="nav-menu">
+        <li class="nav-item"><a href="#" class="nav-link">Accueil</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Jouer</a></li>
+        <li class="logo"><img src="../images/logo.png" alt="logo"></li>
+        <li class="nav-item"><a href="#" class="nav-link">Règles</a></li>
+        <li class="nav-item"><a href="./contact.php" class="nav-link">Contact</a></li>
+    </ul>
+    <div class="hamburger">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </div>
+</nav>
+
+<script src="public/js/contact.js"></script>
