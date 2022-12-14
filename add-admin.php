b56622e01a7793a735a329a5206c4a603c38f0c4
Ambre Kouitini <ambrektn@outlook.fr>
2022-12-14 12:20:13 +0100

    :lipstick: Update ui of add admin page

--- add-admin.php
+++ add-admin.php
@@ -2,11 +2,9 @@
 session_start();
 require_once 'object/user.php';
 require_once 'object/connection.php';
-
 if(isset($_SESSION['function']) && $_SESSION['function'] == 'administrateur'){
     $autorisation = 1;
 }
-
 if($autorisation != 1){
     header('Location: login.php');
 }
@@ -18,31 +16,63 @@ if($autorisation != 1){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <title>Ajouter un utilisateur</title>
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=KoHo:wght@400;500;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/b050931f68.js" crossorigin="anonymous"></script>
+    <script src="public/js/main.js" defer></script>
+    <title>Ajout utilisateur</title>
 </head>
-<body>
-    <div>
-        <form method="POST">
-            <label for="name">Prénom Nom</label>
-            <input type="text" name="name" id="name" placeholder="Jogh DOE">
 
-            <label for="email">Adresse email</label>
-            <input type="email" name="email" id="email" placeholder="mail@gmail.com">
+<body class="add_admin">
+    <?php require_once 'public/includes/_admin-nav.php'; ?>
+    <div class="main_container">
+        <h2>Ajouter un utilisateur</h2>
+        <div class="sub_container">
+            <div class="container_text">
+                <h3> Infos utilisateur</h3>
+                <p>Veuillez renseigner les infos nécessaire à la création de l’utilisateur</p>
+            </div>
+            <form class="add_form" method="POST">
+                <div class="first">
+                    <div class="info_container">
+                        <label for="name">Prénom Nom</label>
+                        <input type="text" name="name" id="name" placeholder="Jogh DOE">
+                    </div>
 
-            <label for="pass">Mot de passe</label>
-            <input type="password" name="password" id="pass" placeholder="Nouveau mot de passe">
+                    <div class="info_container">
+                        <label for="function">Rôle</label>
+                        <div class="select">
+                            <select class="function_select" name="function" id="function">
+                                <option value="interdit">Sélectionnez son rôle...</option>
+                                <option value="autorisé">Autorisé</option>
+                                <option value="administrateur">Administrateur</option>
+                            </select>
+                        </div>
 
-            <label for="function">Rôle</label>
-            <select name="function" id="function">
-                <option value="interdit">Sélectionnez son rôle...</option>
-                <option value="autorisé">Autorisé</option>
-                <option value="administrateur">Administrateur</option>
-            </select>
+                    </div>
+                </div>
+                <div class="second">
+                    <div class="info_container">
+                        <label for="email">Adresse email</label>
+                        <input type="email" name="email" id="email" placeholder="mail@gmail.com">
+                    </div>
 
-            <button type="submit">Ajouter un utilisateur</button>
-        </form>
+                    <div class="info_container">
+                        <label for="pass">Mot de passe</label>
+                        <input type="password" name="password" id="pass" placeholder="Nouveau mot de passe">
+                    </div>
+                </div>
+
+                <div class="button_container">
+                    <button id="submit" type="submit">Ajouter un utilisateur</button>
+                    <a href="dashboard-users.php"><button id="back">Retour</button></a>
+                </div>
+            </form>
+        </div>
     </div>
+
 
     <?php
         if ($_POST) {
