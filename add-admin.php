36acc0a397bc60ae00160022e13cd84e4b6a3854
Nathan Lefetey <lefetey.nathan@gmail.com>
2022-12-14 14:31:13 +0100

    :lipstick: UPDATE add admin page style
    UPDATE global style and responsive

--- add-admin.php
+++ add-admin.php
@@ -2,11 +2,9 @@
 session_start();
 require_once 'object/user.php';
 require_once 'object/connection.php';
-
 if(isset($_SESSION['function']) && $_SESSION['function'] == 'administrateur'){
     $autorisation = 1;
 }
-
 if($autorisation != 1){
     header('Location: login.php');
 }
@@ -19,68 +17,83 @@ if($autorisation != 1){
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="public/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=KoHo:wght@400;500;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/b050931f68.js" crossorigin="anonymous"></script>
     <script src="public/js/main.js" defer></script>
-    <title>Ajouter un utilisateur</title>
+    <title>Ajout utilisateur</title>
 </head>
-<body>
-    <?php require_once 'public/includes/_admin-nav.php' ?>
-    <div class="add-admin">
-        <form class="add-admin__form" method="POST">
-            <div class="add-admin__form__input">
-                <label for="name">Prénom Nom</label>
-                <input type="text" name="name" id="name" placeholder="Jogh DOE">
-            </div>
 
-            <div class="add-admin__form__input">
-                <label for="email">Adresse email</label>
-                <input type="email" name="email" id="email" placeholder="mail@gmail.com">
-            </div>
-
-            <div class="add-admin__form__input">
-                <label for="pass">Mot de passe</label>
-                <input type="password" name="password" id="pass" placeholder="Nouveau mot de passe">
-            </div>
-
-            <div class="add-admin__form__input">
-                <label for="function">Rôle</label>
-                <select name="function" id="function">
-                    <option value="interdit">Sélectionnez son rôle...</option>
-                    <option value="autorisé">Autorisé</option>
-                    <option value="administrateur">Administrateur</option>
-                </select>
-            </div>
-
-            <button class="add-admin__form__submit" type="submit">Ajouter un utilisateur</button>
-        </form>
-        
-        <?php
-            if ($_POST) {
-                $user = new User(
-                    $_POST['name'],
-                    $_POST['email'],
-                    $_POST['password'],
-                    $_POST['function']
-                );
-
-                if ($user->VerifyInput() === true) {
-                    $connection = new Connection();
-                    $verify = $connection->VerifyUser($user);
-
-                    if ($verify === false) {
-                        $insert = $connection->InsertUser($user);
-
-                        if ($insert) { ?>
-                            <h2>Création réussie</h2>
-                        <?php } else { ?>
-                            <h2>Un problème est survenu</h2>
-                        <?php }
-                    } else { ?>
-                        <h2>Adresse email déjà utilisée</h2>
-                    <?php }
-                }
-            }
-        ?>
+<body class="add-admin">
+    <?php require_once 'public/includes/_admin-nav.php'; ?>
+    <div class="add-admin__header">
+        <h2>Ajouter un utilisateur</h2>
+        <a href="dashboard-users.php">Retour</a>
     </div>
+    <div class="add-admin__container">
+        <div class="add-admin__container__title">
+            <h3> Infos utilisateur</h3>
+            <p>Veuillez renseigner les infos nécessaire à la création de l’utilisateur</p>
+        </div>
+        <form class="add-admin__container__form" method="POST">
+            <div class="add-admin__container__form__first">
+                <div class="add-admin__container__form__first__input">
+                    <label for="name">Prénom Nom</label>
+                    <input type="text" name="name" id="name" placeholder="Jogh DOE">
+                </div>
 
+                <div class="add-admin__container__form__first__input">
+                    <label for="function">Rôle</label>
+                    <select class="function_select" name="function" id="function">
+                        <option value="interdit">Sélectionnez son rôle...</option>
+                        <option value="autorisé">Autorisé</option>
+                        <option value="administrateur">Administrateur</option>
+                    </select>
+                </div>
+            </div>
+            <div class="add-admin__container__form__second">
+                <div class="add-admin__container__form__second__input">
+                    <label for="email">Adresse email</label>
+                    <input type="email" name="email" id="email" placeholder="mail@gmail.com">
+                </div>
+
+                <div class="add-admin__container__form__second__input">
+                    <label for="pass">Mot de passe</label>
+                    <input type="password" name="password" id="pass" placeholder="Nouveau mot de passe">
+                </div>
+            </div>
+
+            <button class="add-admin__container__form__submit" id="submit" type="submit">Ajouter un utilisateur</button>
+        </form>
+    </div>
+    
+    <?php
+        if ($_POST) {
+            $user = new User(
+                $_POST['name'],
+                $_POST['email'],
+                $_POST['password'],
+                $_POST['function']
+            );
+
+            if ($user->VerifyInput() === true) {
+                $connection = new Connection();
+                $verify = $connection->VerifyUser($user);
+
+                if ($verify === false) {
+                    $insert = $connection->InsertUser($user);
+
+                    if ($insert) { ?>
+                        <h2>Création réussie</h2>
+                    <?php } else { ?>
+                        <h2>Un problème est survenu</h2>
+                    <?php }
+                } else { ?>
+                    <h2>Adresse email déjà utilisée</h2>
+                <?php }
+            }
+        }
+    ?>
 </body>
 </html>
--- public/includes/ingame-navbar.php
+++ public/includes/ingame-navbar.php
@@ -1,5 +1,6 @@
 <nav class="gm-navbar">
     <h3 class="logo-phone-navbar">CARBON</h3>
+    <img class="logo-ordi-navbar" src="images/logos/logo.png" alt="">
     <div class="quit-button">
         <a href=""><img src="./images/icons/out.png" alt="Quit"></a>
     </div>
