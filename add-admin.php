fa9c245ad56e7f7254fa14fac19e128943f7f515
Nathan Lefetey <lefetey.nathan@gmail.com>
2022-12-14 09:23:33 +0100

    :lipstick: UPDATE style

--- add-admin.php
+++ add-admin.php
@@ -18,58 +18,69 @@ if($autorisation != 1){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="public/css/style.css">
+    <script src="public/js/main.js" defer></script>
     <title>Ajouter un utilisateur</title>
 </head>
 <body>
-    <div>
-        <form method="POST">
-            <label for="name">Prénom Nom</label>
-            <input type="text" name="name" id="name" placeholder="Jogh DOE">
+    <?php require_once 'public/includes/_admin-nav.php' ?>
+    <div class="add-admin">
+        <form class="add-admin__form" method="POST">
+            <div class="add-admin__form__input">
+                <label for="name">Prénom Nom</label>
+                <input type="text" name="name" id="name" placeholder="Jogh DOE">
+            </div>
 
-            <label for="email">Adresse email</label>
-            <input type="email" name="email" id="email" placeholder="mail@gmail.com">
+            <div class="add-admin__form__input">
+                <label for="email">Adresse email</label>
+                <input type="email" name="email" id="email" placeholder="mail@gmail.com">
+            </div>
 
-            <label for="pass">Mot de passe</label>
-            <input type="password" name="password" id="pass" placeholder="Nouveau mot de passe">
+            <div class="add-admin__form__input">
+                <label for="pass">Mot de passe</label>
+                <input type="password" name="password" id="pass" placeholder="Nouveau mot de passe">
+            </div>
 
-            <label for="function">Rôle</label>
-            <select name="function" id="function">
-                <option value="interdit">Sélectionnez son rôle...</option>
-                <option value="autorisé">Autorisé</option>
-                <option value="administrateur">Administrateur</option>
-            </select>
+            <div class="add-admin__form__input">
+                <label for="function">Rôle</label>
+                <select name="function" id="function">
+                    <option value="interdit">Sélectionnez son rôle...</option>
+                    <option value="autorisé">Autorisé</option>
+                    <option value="administrateur">Administrateur</option>
+                </select>
+            </div>
 
-            <button type="submit">Ajouter un utilisateur</button>
+            <button class="add-admin__form__submit" type="submit">Ajouter un utilisateur</button>
         </form>
+        
+        <?php
+            if ($_POST) {
+                $user = new User(
+                    $_POST['name'],
+                    $_POST['email'],
+                    $_POST['password'],
+                    $_POST['function']
+                );
+
+                if ($user->VerifyInput() === true) {
+                    $connection = new Connection();
+                    $verify = $connection->VerifyUser($user);
+
+                    if ($verify === false) {
+                        $insert = $connection->InsertUser($user);
+
+                        if ($insert) { ?>
+                            <h2>Création réussie</h2>
+                        <?php } else { ?>
+                            <h2>Un problème est survenu</h2>
+                        <?php }
+                    } else { ?>
+                        <h2>Adresse email déjà utilisée</h2>
+                    <?php }
+                }
+            }
+        ?>
     </div>
 
-    <?php
-        if ($_POST) {
-            $user = new User(
-                $_POST['name'],
-                $_POST['email'],
-                $_POST['password'],
-                $_POST['function']
-            );
-
-            if ($user->VerifyInput() === true) {
-                $connection = new Connection();
-                $verify = $connection->VerifyUser($user);
-
-                if ($verify === false) {
-                    $insert = $connection->InsertUser($user);
-
-                    if ($insert) { ?>
-                        <h2>Création réussie</h2>
-                    <?php } else { ?>
-                        <h2>Un problème est survenu</h2>
-                    <?php }
-                } else { ?>
-                    <h2>Adresse email déjà utilisée</h2>
-                <?php }
-            }
-        }
-    ?>
 </body>
 </html>
