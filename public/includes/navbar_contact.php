2883bb6e8027afee046f8e2f53b6b0165cbfeada
Nathan Lefetey <lefetey.nathan@gmail.com>
2022-12-16 09:49:23 +0100

    :lipstick: FIX css bug

--- public/includes/navbar_contact.php
+++ public/includes/navbar_contact.php
@@ -1,12 +1,11 @@
 <nav class="navbar">
     <div class="container-logo">
-        <img class="logo" src="../images/logos/logo.png" alt="">
         <h2 class="brandName">CARBON</h2>
     </div>
     <ul class="nav-menu">
         <li class="nav-item"><a href="#" class="nav-link">Accueil</a></li>
         <li class="nav-item"><a href="#" class="nav-link">Jouer</a></li>
-        <li class="logo"><img class="logoimg" src="../images/logos/logo.png" alt="logo"></li>
+        <li class="logo"><img class="logoimg" src="images/logos/logo.png" alt="logo"></li>
         <li class="nav-item"><a href="#" class="nav-link">Règles</a></li>
         <li class="nav-item"><a href="./contact.php" class="nav-link">Contact</a></li>
     </ul>
